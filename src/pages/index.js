e569e71a9872f0a5b69c3773fdf1cc2a10f0de00
Drailan John D. Terrible <drailanjohn.gss@gmail.com>
2019-01-11 15:34:10 +0800

    implemented index for blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -15,7 +15,27 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <h2>Index</h2>
+    {data.allMarkdownRemark.edges.map(post => (
+      <Link key={post.node.id} to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
+    ))}
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
